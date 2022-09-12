40bba5c6bd8b984262f362f1c1827eada56135a5
omersari <omersari@teknasyon.com>
2022-09-12 11:12:48 +0300

    lint

--- array/best-time-to-buy-and-sell/solution.ts
+++ array/best-time-to-buy-and-sell/solution.ts
@@ -2,8 +2,8 @@ export default function maxProfit(prices: number[]): number {
   let maxProfit = 0;
   let minPrice = prices[0];
   for (let i = 1; i < prices.length; i++) {
-    let sellPrice = prices[i];
-    let profit = sellPrice - minPrice;
+    const sellPrice = prices[i];
+    const profit = sellPrice - minPrice;
 
     maxProfit = Math.max(maxProfit, profit);
 
